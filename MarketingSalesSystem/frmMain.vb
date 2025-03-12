a51f0e93a0ec17b2d1b988fbf4b16f8768669f94
WiceCake <alburoto.168754@gensan.sti.edu.ph>
2025-03-12 13:22:43 +0800

    init

---
+++ MarketingSalesSystem/frmMain.vb
@@ -0,0 +1,12 @@
+﻿Imports System.ComponentModel
+Imports System.Text
+
+
+Partial Public Class frmMain
+    Shared Sub New()
+        DevExpress.UserSkins.BonusSkins.Register()
+    End Sub
+    Public Sub New()
+        InitializeComponent()
+    End Sub
+End Class
