9c9cb0e84ee6ba27e1d9ddaf7689736e244246ce
WiceCake <alburoto.168754@gensan.sti.edu.ph>
2025-03-13 09:26:09 +0800

    adding models

---
+++ MarketingSalesSystem/Models/SalesReport.vb
@@ -0,0 +1,17 @@
+﻿Public Class SalesReport
+    Public salesReport_ID As Integer
+    Public salesDate As Date?
+    Public salesNum As String
+    Public sellingType As String
+    Public unloadingType As String
+    Public unloadingVessel_ID As Integer
+    Public unloadingForeignVessel As String
+    Public buyer As String
+    Public catchtDeliveryNum As String
+    Public usdRate As Decimal
+    Public contractNum As String
+    Public remarks As String
+    Public encodedBy As Integer
+    Public encodedOn As Date
+    Public approvalStatus As Integer
+End Class
