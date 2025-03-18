91975d808ea85cf8da035807b72c94dc63dd7670
WiceCake <alburoto.168754@gensan.sti.edu.ph>
2025-03-18 10:37:26 +0800

    creating weight-slip

---
+++ MarketingSalesSystem/Views/ucWeightSlip.vb
@@ -0,0 +1,43 @@
+﻿Imports DevExpress.XtraTab
+Imports DevExpress.XtraGrid
+Imports DevExpress.XtraLayout
+
+Public Class ucWeightSlip
+    Inherits ucBase
+
+    Private tabControl As XtraTabControl
+
+    Sub New(ByVal title As String)
+        InitializeComponent()
+
+        MyBase.title = title
+        LabelControl1.Text = title
+
+
+        Dim grid = New GridControl() With {
+            .Dock = DockStyle.Fill
+        }
+
+        LayoutControl2.Controls.Add(grid)
+
+        Dim layoutItem As LayoutControlItem = LayoutControl2.AddItem("", grid)
+        layoutItem.TextVisible = False
+    End Sub
+
+    Function setTab(ByRef tab As XtraTabPage) As GridControl
+        Dim layout = New LayoutControl With {
+                        .Dock = DockStyle.Fill
+                    }
+        tab.Controls.Add(layout)
+        Dim grid = New GridControl() With {
+            .Dock = DockStyle.Fill
+        }
+        layout.Controls.Add(grid)
+        Dim layoutItem As LayoutControlItem = layout.AddItem("", grid)
+        layoutItem.TextVisible = False
+
+        Return grid
+    End Function
+
+
+End Class
