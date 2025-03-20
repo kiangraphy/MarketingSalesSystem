70cc72a0152ada5e891539d0d7beb8ee2116758c
WiceCake <alburoto.168754@gensan.sti.edu.ph>
2025-03-20 16:29:01 +0800

    Created Grid

--- MarketingSalesSystem/Controllers/ctrlSales.vb
+++ MarketingSalesSystem/Controllers/ctrlSales.vb
@@ -25,7 +25,12 @@ Public Class ctrlSales
 
         frmSI = New frm_salesInvoice(Me)
 
+        initSalesDataTable()
+
+        frmSI.GridControl1.DataSource = frmSI.dt
+
         With frmSI
+            loadRows()
             .cmbBuyer.Enabled = False
             .txtBuyer.Enabled = False
             .lcmbBuyer.Visibility = Utils.LayoutVisibility.Never
@@ -34,6 +39,68 @@ Public Class ctrlSales
             generateCombo()
             .Show()
         End With
+    End Sub
+
+    Sub initSalesDataTable()
+        With frmSI
+            .dt = New DataTable()
+
+            .dt.Columns.Add("Class", GetType(String))
+            .dt.Columns.Add("Size", GetType(String))
+            .dt.Columns.Add("Price", GetType(Double))
+            .dt.Columns.Add("AUK_Catcher1", GetType(Double))
+            .dt.Columns.Add("AUK_Catcher2", GetType(Double))
+            .dt.Columns.Add("AUK_Total", GetType(Double))
+            .dt.Columns.Add("AUA_Catcher1", GetType(Double))
+            .dt.Columns.Add("AUA_Catcher2", GetType(Double))
+            .dt.Columns.Add("AUA_Total", GetType(Double))
+            .dt.Columns.Add("SK_Catcher1", GetType(Double))
+            .dt.Columns.Add("SK_Catcher2", GetType(Double))
+            .dt.Columns.Add("SK_Total", GetType(Double))
+            .dt.Columns.Add("SA_Catcher1", GetType(Double))
+            .dt.Columns.Add("SA_Catcher2", GetType(Double))
+            .dt.Columns.Add("SA_Total", GetType(Double))
+            .dt.Columns.Add("NK_Catcher1", GetType(Double))
+            .dt.Columns.Add("NK_Catcher2", GetType(Double))
+            .dt.Columns.Add("NK_Total", GetType(Double))
+            .dt.Columns.Add("NA_Catcher1", GetType(Double))
+            .dt.Columns.Add("NA_Catcher2", GetType(Double))
+            .dt.Columns.Add("NA_Total", GetType(Double))
+
+
+        End With
+    End Sub
+
+    Sub loadRows()
+        Dim dr As DataRow
+
+        Dim skipjack = New String() {"0.300 - 0.499", "0.500 - 0.999", "1.0 - 1.39", "1.4 - 1.79",
+                                     "1.8 - 2.49", "2.5 - 3.49", "3.5 - UP"}
+        Dim yellowfin = New String() {"0.300 - 0.499", "0.500 - 0.999", "1.0 - 1.49", "1.5 - 2.49",
+                                     "2.5 - 3.49", "3.5 - 4.99", "5.0 - 9.99", "10  - UP GOOD", "10 - UP DEFORMED"}
+        Dim bigeye = New String() {"0.500 - 0.999", "1.0 - 1.49", "1.5 - 2.49", "2.5 - 3.49", "3.5 - 4.99",
+                                   "5.0 - 9.99", "10 -  UP"}
+        For Each sj In skipjack
+            dr = frmSI.dt.NewRow()
+            dr("Class") = "SKIPJACK"
+            dr("Size") = sj
+            frmSI.dt.Rows.Add(dr)
+        Next
+        For Each yf In yellowfin
+            dr = frmSI.dt.NewRow()
+            dr("Class") = "YELLOWFIN"
+            dr("Size") = yf
+            frmSI.dt.Rows.Add(dr)
+        Next
+
+        dr = frmSI.dt.NewRow()
+        dr("Class") = "BONITO"
+        dr("Size") = "ALL SIZES"
+        frmSI.dt.Rows.Add(dr)
+        dr = frmSI.dt.NewRow()
+        dr("Class") = "FISHMEAL"
+        dr("Size") = "ALL SIZES"
+        frmSI.dt.Rows.Add(dr)
     End Sub
 
     Sub generateCombo()
